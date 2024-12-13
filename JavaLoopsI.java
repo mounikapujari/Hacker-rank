c76fcbe66eb0ce866fb28b069e9a13bd1dc4703a
mounikapujari <mounikapujari2005@gmail.com>
2024-12-13 23:01:15 +0530

    Add files via upload

---
+++ JavaLoopsI.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        for(int i=1;i<=10;i++){
+            System.out.println(N+" x "+i+" = "+(N*i));
+        }
+    }
+}
